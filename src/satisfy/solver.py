31edd9a8b60cca18c2f7f4fc12f5e1029dd372d7
Simone Campagna <simone.campagna11@gmail.com>
2019-06-30 11:25:54 +0200

    bug fix

--- src/satisfy/solver.py
+++ src/satisfy/solver.py
@@ -196,15 +196,18 @@ class Solver(object):
         elif var_selection_policy is VarSelectionPolicy.REVERSED:
             var_names = tuple(reversed(var_names))
         elif var_selection_policy is VarSelectionPolicy.MIN_BOUND:
-            # var_names = tuple(sorted(var_names, key=lambda x: len(var_constraints[x]), reverse=False))
+            # var_names = tuple(sorted(var_names, key=lambda x: len(var_constraints[x]), reverse=True))
             var_names = tuple(sorted(var_names, key=lambda x: len(model.get_var_domain(x))))
         elif var_selection_policy is VarSelectionPolicy.MAX_BOUND:
-            # var_names = tuple(sorted(var_names, key=lambda x: len(var_constraints[x]), reverse=False))
+            # var_names = tuple(sorted(var_names, key=lambda x: len(var_constraints[x]), reverse=True))
             var_names = tuple(sorted(var_names, key=lambda x: len(model.get_var_domain(x)), reverse=True))
         elif var_selection_policy is VarSelectionPolicy.MIN_CONSTRAINT:
             var_names = tuple(sorted(var_names, key=lambda x: len(var_constraints[x])))
         elif var_selection_policy is VarSelectionPolicy.MAX_CONSTRAINT:
             var_names = tuple(sorted(var_names, key=lambda x: len(var_constraints[x]), reverse=True))
+
+        # for var_name in var_names:
+        #     print(var_name, len(model.get_var_domain(var_name)), len(var_constraints[var_name]))
 
         if not var_names:
             return
@@ -282,122 +285,122 @@ class Solver(object):
                 timer.start()
                 continue
 
-    def _solve_forbidden(self, model, **args):
-        timeout = args.get('timeout', self._timeout)
-        var_selection_policy = args.get('var_selection_policy', self._var_selection_policy)
-        additional_constraints = args.get('additional_constraints', ())
-        # print("::: timeout=", timeout)
-        # print("::: var_selection_policy=", var_selection_policy)
-        # print("::: additional_constraints=", additional_constraints)
-
-        model_info = self._setup(model, additional_constraints=additional_constraints)
-        variables = model_info.variables
-        var_constraints = model_info.var_constraints
-        var_ad_constraints = model_info.var_ad_constraints
-        var_names = tuple(variables)
-        if var_selection_policy is VarSelectionPolicy.GROUPS:
-            var_bounds = model_info.var_bounds
-            var_sizes = model_info.var_sizes
-            var_groups = model_info.var_groups
-            groups = model_info.groups
-            group_bounds = model_info.group_bounds
-            group_sizes = model_info.group_sizes
-
-            sort_group = lambda x: (-group_bounds[x], -group_sizes[x])
-            group_prio = {groupid: groupid for groupid in groups}  #idx for idx, groupid in enumerate(sorted(groups, key=lambda x: (group_bounds[x], group_sizes[x]), reverse=True))}
-            var_group_prio = {}
-            for var_name in var_names:
-                if var_groups[var_name]:
-                    var_group_prio[var_name] = min(group_prio[groupid] for groupid in var_groups[var_name])
-                else:
-                    var_group_prio[var_name] = -1
-            sort_key = lambda x: (var_group_prio[x], var_bounds[x], var_sizes[x])
-            s_var_names = tuple(sorted(variables, key=sort_key, reverse=True))
-            var_names = tuple(reversed(s_var_names))
-        elif var_selection_policy is VarSelectionPolicy.RANDOM:
-            var_names = list(var_names)
-            random.shuffle(var_names)
-            var_names = tuple(var_names)
-        elif var_selection_policy is VarSelectionPolicy.REVERSED:
-            var_names = tuple(reversed(var_names))
-
-        # print("::: var_names=", var_names)
-        # for var_name in var_names:
-        #     print("::: var_name={}".format(var_name))
-        #     print(":::     domain={}".format(variables[var_name].domain))
-        #     print(":::     var_constraints: [#{}]".format(len(model_info.var_constraints.get(var_name, []))))
-        #     for c in model_info.var_constraints.get(var_name, []):
-        #         print(":::         {}".format(c))
-
-        stack = []
-        domains = {}
-        forbidden = {var_name: {} for var_name in var_names}
-        stack.append((var_names, {}))
-
-        timer = Timer()
-        timer.start()
-        while stack:
-            if timeout is not None:
-                cur_elapsed = timer.elapsed()
-                if cur_elapsed > timeout:
-                    timer.abort()
-                    self._interrupt = "timeout"
-                    return
-
-            unbound_vars, substitution = stack[-1]
-
-            var_name, unbound_vars = unbound_vars[0], unbound_vars[1:]
-
-            substitution = substitution.copy()
-
-            for c_var in var_ad_constraints[var_name]:
-                forbidden[c_var].pop(var_name, None)
-
-            domain = domains.get(var_name, None)
-            if domain is None:
-                fset = set(forbidden[var_name].values())
-                domain = []
-                for value in variables[var_name].domain:
-                    if value not in fset:
-                        substitution[var_name] = value
-                        for constraint in var_constraints[var_name]:
-                            if constraint.unsatisfied(substitution):
-                                # var_name == value is *not* acceptable
-                                # because it breaks the Constraint
-                                break
-                        else:
-                            # var_name == value is acceptable
-                            domain.append(value)
-                if domain:
-                    domains[var_name] = domain
-                else:
-                    stack.pop(-1)
-                    continue
-            elif not domain:
-                domains.pop(var_name)
-                # for c_var in var_ad_constraints[var_name]:
-                #     forbidden[c_var].pop(var_name, None)
-                stack.pop(-1)
-                continue
-
-            value = domain.pop(0)
-            substitution[var_name] = value
-
-            if unbound_vars:
-                for c_var in var_ad_constraints[var_name].intersection(unbound_vars):
-                    forbidden[c_var][var_name] = value
-                stack.append((unbound_vars, substitution))
-            else:
-                timer.stop()
-                # for constraint in model.constraints():
-                #     print(constraint.evaluate(substitution), constraint)
-                yield substitution
-                if limit is not None and num_solutions >= limit:
-                    timer.abort()
-                    self._interrupt = "limit"
-                    return
-                timer.start()
-                continue
+# REM     def _solve_forbidden(self, model, **args):
+# REM         timeout = args.get('timeout', self._timeout)
+# REM         var_selection_policy = args.get('var_selection_policy', self._var_selection_policy)
+# REM         additional_constraints = args.get('additional_constraints', ())
+# REM         # print("::: timeout=", timeout)
+# REM         # print("::: var_selection_policy=", var_selection_policy)
+# REM         # print("::: additional_constraints=", additional_constraints)
+# REM 
+# REM         model_info = self._setup(model, additional_constraints=additional_constraints)
+# REM         variables = model_info.variables
+# REM         var_constraints = model_info.var_constraints
+# REM         var_ad_constraints = model_info.var_ad_constraints
+# REM         var_names = tuple(variables)
+# REM         if var_selection_policy is VarSelectionPolicy.GROUPS:
+# REM             var_bounds = model_info.var_bounds
+# REM             var_sizes = model_info.var_sizes
+# REM             var_groups = model_info.var_groups
+# REM             groups = model_info.groups
+# REM             group_bounds = model_info.group_bounds
+# REM             group_sizes = model_info.group_sizes
+# REM 
+# REM             sort_group = lambda x: (-group_bounds[x], -group_sizes[x])
+# REM             group_prio = {groupid: groupid for groupid in groups}  #idx for idx, groupid in enumerate(sorted(groups, key=lambda x: (group_bounds[x], group_sizes[x]), reverse=True))}
+# REM             var_group_prio = {}
+# REM             for var_name in var_names:
+# REM                 if var_groups[var_name]:
+# REM                     var_group_prio[var_name] = min(group_prio[groupid] for groupid in var_groups[var_name])
+# REM                 else:
+# REM                     var_group_prio[var_name] = -1
+# REM             sort_key = lambda x: (var_group_prio[x], var_bounds[x], var_sizes[x])
+# REM             s_var_names = tuple(sorted(variables, key=sort_key, reverse=True))
+# REM             var_names = tuple(reversed(s_var_names))
+# REM         elif var_selection_policy is VarSelectionPolicy.RANDOM:
+# REM             var_names = list(var_names)
+# REM             random.shuffle(var_names)
+# REM             var_names = tuple(var_names)
+# REM         elif var_selection_policy is VarSelectionPolicy.REVERSED:
+# REM             var_names = tuple(reversed(var_names))
+# REM 
+# REM         # print("::: var_names=", var_names)
+# REM         # for var_name in var_names:
+# REM         #     print("::: var_name={}".format(var_name))
+# REM         #     print(":::     domain={}".format(variables[var_name].domain))
+# REM         #     print(":::     var_constraints: [#{}]".format(len(model_info.var_constraints.get(var_name, []))))
+# REM         #     for c in model_info.var_constraints.get(var_name, []):
+# REM         #         print(":::         {}".format(c))
+# REM 
+# REM         stack = []
+# REM         domains = {}
+# REM         forbidden = {var_name: {} for var_name in var_names}
+# REM         stack.append((var_names, {}))
+# REM 
+# REM         timer = Timer()
+# REM         timer.start()
+# REM         while stack:
+# REM             if timeout is not None:
+# REM                 cur_elapsed = timer.elapsed()
+# REM                 if cur_elapsed > timeout:
+# REM                     timer.abort()
+# REM                     self._interrupt = "timeout"
+# REM                     return
+# REM 
+# REM             unbound_vars, substitution = stack[-1]
+# REM 
+# REM             var_name, unbound_vars = unbound_vars[0], unbound_vars[1:]
+# REM 
+# REM             substitution = substitution.copy()
+# REM 
+# REM             for c_var in var_ad_constraints[var_name]:
+# REM                 forbidden[c_var].pop(var_name, None)
+# REM 
+# REM             domain = domains.get(var_name, None)
+# REM             if domain is None:
+# REM                 fset = set(forbidden[var_name].values())
+# REM                 domain = []
+# REM                 for value in variables[var_name].domain:
+# REM                     if value not in fset:
+# REM                         substitution[var_name] = value
+# REM                         for constraint in var_constraints[var_name]:
+# REM                             if constraint.unsatisfied(substitution):
+# REM                                 # var_name == value is *not* acceptable
+# REM                                 # because it breaks the Constraint
+# REM                                 break
+# REM                         else:
+# REM                             # var_name == value is acceptable
+# REM                             domain.append(value)
+# REM                 if domain:
+# REM                     domains[var_name] = domain
+# REM                 else:
+# REM                     stack.pop(-1)
+# REM                     continue
+# REM             elif not domain:
+# REM                 domains.pop(var_name)
+# REM                 # for c_var in var_ad_constraints[var_name]:
+# REM                 #     forbidden[c_var].pop(var_name, None)
+# REM                 stack.pop(-1)
+# REM                 continue
+# REM 
+# REM             value = domain.pop(0)
+# REM             substitution[var_name] = value
+# REM 
+# REM             if unbound_vars:
+# REM                 for c_var in var_ad_constraints[var_name].intersection(unbound_vars):
+# REM                     forbidden[c_var][var_name] = value
+# REM                 stack.append((unbound_vars, substitution))
+# REM             else:
+# REM                 timer.stop()
+# REM                 # for constraint in model.constraints():
+# REM                 #     print(constraint.evaluate(substitution), constraint)
+# REM                 yield substitution
+# REM                 if limit is not None and num_solutions >= limit:
+# REM                     timer.abort()
+# REM                     self._interrupt = "limit"
+# REM                     return
+# REM                 timer.start()
+# REM                 continue
 
     def optimize(self, model, objective, **args):
         if isinstance(objective, Objective):
